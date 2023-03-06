9364afe44bd7493bc0f93e29d62e941d56be4e22
andryushchenkoka <kirill2goo@gmail.com>
2023-03-07 01:06:26 +0300

    Добавление скриншотов

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -2,14 +2,21 @@ package tests;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
+
 import drivers.BrowserstackDriver;
 import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 
 import static com.codeborne.selenide.Selenide.*;
+import static io.appium.java_client.AppiumBy.id;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class BaseTest {
 
@@ -23,12 +30,19 @@ public class BaseTest {
     void addListener() {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
         open();
+        $(id("org.wikipedia:id/fragment_onboarding_skip_button")).click();
     }
 
     @AfterEach
     void addAttachments() {
+
+        DateFormat dateFormat = new SimpleDateFormat("yyyy_MM_dd_HH_mm_ss");
+        Date date = new Date();
+
+
         String sessionId = sessionId().toString();
         Attach.pageSource();
+        Attach.screenshotAs("screenshot_" + dateFormat.format(date));
         closeWebDriver();
         Attach.addVideo(sessionId);
     }
