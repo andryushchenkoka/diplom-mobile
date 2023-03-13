5fe9d93153cd11681281fa666bb267cdba33ddeb
andryushchenkoka <kirill2goo@gmail.com>
2023-03-13 11:21:47 +0300

    basic auth browserstack

--- src/test/java/helpers/Browserstack.java
+++ src/test/java/helpers/Browserstack.java
@@ -1,5 +1,8 @@
 package helpers;
 
+import config.BrowserstackConfig;
+import org.aeonbits.owner.ConfigFactory;
+
 import static helpers.CustomApiListener.withCustomTemplates;
 import static io.restassured.RestAssured.given;
 import static java.lang.String.format;
@@ -7,11 +10,11 @@ import static java.lang.String.format;
 public class Browserstack {
     public static String getVideoUrl(String sessionId) {
         String url = format("https://api.browserstack.com/app-automate/sessions/%s.json", sessionId);
-
+        BrowserstackConfig config = ConfigFactory.create(BrowserstackConfig.class);
         return given()
                 .log().all()
                 .filter(withCustomTemplates())
-                .auth().basic("", "")
+                .auth().basic(config.getBsUser(), config.getBsKey())
                 .when()
                 .get(url)
                 .then()
